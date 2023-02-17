da81a785874c623dfdd590067644138b555bf81b
mohammad sakib <sakibmohammad7679@gmail.com>
2023-02-18 01:53:26 +0600

    parallelogram and rhombus card calculation added

--- js/geometry.js
+++ js/geometry.js
@@ -28,6 +28,7 @@ document.getElementById('triangle-btn').addEventListener('click', function(){
     }
 })
 // triangle calculation end here
+//rectangle calculation start here
 document.getElementById('rectangle-btn').addEventListener('click', function(){
     const rectangleWidthValue = inputFieldValue('rectangle-w');
     const rectanglelengthValue = inputFieldValue('rectangle-l');
@@ -45,3 +46,42 @@ document.getElementById('rectangle-btn').addEventListener('click', function(){
     rectangleNameSet.innerText = rectangleName;
     }
 })
+//rectangle calculation end here
+//Parallelogram calculation start here
+document.getElementById('parallelogram-btn').addEventListener('click', function(){
+    const ParallelogramBaseValue = inputFieldValue('parallelogram-b');
+    const ParallelogramhightValue = inputFieldValue('parallelogram-h');
+    if(isNaN(ParallelogramBaseValue) || ParallelogramBaseValue <= 0 || isNaN(ParallelogramhightValue) || ParallelogramhightValue <= 0 ){
+        alert('Please Enter the valid value');
+    }
+    else{
+        const parallelogramResult = ParallelogramBaseValue * ParallelogramhightValue;
+
+    const parallelogramResultSet = document.getElementById('parallelogram-result');
+    parallelogramResultSet.innerText = parallelogramResult;
+
+    const parallelogramName = textFieldValue('parallelogram-name');
+    const parallelogramNameSet = document.getElementById('parallelogram-name-set');
+    parallelogramNameSet.innerText = parallelogramName;
+    }
+})
+//Parallelogram calculation end here
+//Parallelogram calculation start here
+document.getElementById('rhombus-btn').addEventListener('click', function(){
+    const rhombusD1Value = inputFieldValue('rhombus-b');
+    const rhombusD2Value = inputFieldValue('rhombus-h');
+    if(isNaN(rhombusD1Value) || rhombusD1Value <= 0 || isNaN(rhombusD2Value) || rhombusD1Value <= 0 ){
+        alert('Please Enter the valid value');
+    }
+    else{
+        const rhombusResult = 0.5*(rhombusD1Value * rhombusD1Value);
+
+    const rhombusResultSet = document.getElementById('rhombus-result');
+    rhombusResultSet.innerText = rhombusResult;
+
+    const rhombusName = textFieldValue('rhombus-name');
+    const rhombusNameSet = document.getElementById('rhombus-name-set');
+    rhombusNameSet.innerText = rhombusName;
+    }
+})
+//Parallelogram calculation end here
